6f2fa1ebb299b31492a211711c0ca80d50c5ff6b
TommasoGalistu <tommasogalistu92@gmail.com>
2024-06-20 16:07:37 +0200

    func: for add post dynamically

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
+let containerPost = document.querySelector('.post')
+
 const posts = [
     {
         "id": 1,
@@ -55,3 +57,39 @@ const posts = [
         "created": "2021-03-05"
     }
 ];
+
+
+
+// inserisco le tutte le immagini post in maniera dinamica
+posts.forEach(personPost =>{
+    containerPost.innerHTML += `
+    <div class="post__header">
+                <div class="post-meta">                    
+                    <div class="post-meta__icon">
+                        <img class="profile-pic" src="${personPost.author.image}" alt="${personPost.author.name}">                    
+                    </div>
+                    <div class="post-meta__data">
+                        <div class="post-meta__author">${personPost.author.name}</div>
+                        <div class="post-meta__time">${personPost.created}</div>
+                    </div>                    
+                </div>
+            </div>
+            <div class="post__text">${personPost.content}</div>
+            <div class="post__image">
+                <img src="${personPost.media}" alt="">
+            </div>
+            <div class="post__footer">
+                <div class="likes js-likes">
+                    <div class="likes__cta">
+                        <a class="like-button  js-like-button" href="#" data-postid="1">
+                            <i class="like-button__icon fas fa-thumbs-up" aria-hidden="true"></i>
+                            <span class="like-button__label">Mi Piace</span>
+                        </a>
+                    </div>
+                    <div class="likes__counter">
+                        Piace a <b id="like-counter-1" class="js-likes-counter">${personPost.likes}</b> persone
+                    </div>
+                </div> 
+            </div>`
+});
+
