c3343d4856c05b0a30f65e6f3c5c66d55dfebdc0
TommasoGalistu <tommasogalistu92@gmail.com>
2024-06-20 16:26:49 +0200

    func: click mi piace

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-let containerPost = document.querySelector('.post')
+let containerPost = document.getElementById('container')
 
 const posts = [
     {
@@ -57,39 +57,63 @@ const posts = [
         "created": "2021-03-05"
     }
 ];
-
-
+// esempio di post oggetto
+// {
+//     "id": 1,
+//     "content": "Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.",
+//     "media": "https://unsplash.it/600/300?image=171",
+//     "author": {
+//         "name": "Phil Mangione",
+//         "image": "https://unsplash.it/300/300?image=15"
+//     },
+//     "likes": 80,
+//     "created": "2021-06-25"
+// },
 
 // inserisco le tutte le immagini post in maniera dinamica
-posts.forEach(personPost =>{
+posts.forEach(personPost => {
     containerPost.innerHTML += `
-    <div class="post__header">
-                <div class="post-meta">                    
-                    <div class="post-meta__icon">
-                        <img class="profile-pic" src="${personPost.author.image}" alt="${personPost.author.name}">                    
+    <div class="post">
+        <div class="post__header">
+                    <div class="post-meta">                    
+                        <div class="post-meta__icon">
+                            <img class="profile-pic" src="${personPost.author.image}" alt="${personPost.author.name}">                    
+                        </div>
+                        <div class="post-meta__data">
+                            <div class="post-meta__author">${personPost.author.name}</div>
+                            <div class="post-meta__time">${personPost.created}</div>
+                        </div>                    
                     </div>
-                    <div class="post-meta__data">
-                        <div class="post-meta__author">${personPost.author.name}</div>
-                        <div class="post-meta__time">${personPost.created}</div>
-                    </div>                    
                 </div>
-            </div>
-            <div class="post__text">${personPost.content}</div>
-            <div class="post__image">
-                <img src="${personPost.media}" alt="">
-            </div>
-            <div class="post__footer">
-                <div class="likes js-likes">
-                    <div class="likes__cta">
-                        <a class="like-button  js-like-button" href="#" data-postid="1">
-                            <i class="like-button__icon fas fa-thumbs-up" aria-hidden="true"></i>
-                            <span class="like-button__label">Mi Piace</span>
-                        </a>
+                <div class="post__text">${personPost.content}</div>
+                <div class="post__image">
+                    <img src="${personPost.media}" alt="">
+                </div>
+                <div class="post__footer">
+                    <div class="likes js-likes">
+                        <div class="likes__cta">
+                            <a class="like-button js-like-button" href="#" data-postid="1">
+                                <i class="like-button__icon fas fa-thumbs-up" aria-hidden="true"></i>
+                                <span class="like-button__label">Mi Piace</span>
+                            </a>
+                        </div>
+                        <div class="likes__counter">
+                            Piace a <b id="like-counter-1" class="js-likes-counter">${personPost.likes}</b> persone
+                        </div>
                     </div>
-                    <div class="likes__counter">
-                        Piace a <b id="like-counter-1" class="js-likes-counter">${personPost.likes}</b> persone
-                    </div>
-                </div> 
-            </div>`
+                </div>
+        </div> 
+    </div>
+                `
 });
+
+
+// trovo il mi piace e cambio la classe 
+containerPost.addEventListener('click', (e) => {
+    
+    e.target.classList.remove('like-button');
+    e.target.classList.add('like-button--liked');
+
+});
+
 
